76e9521d498b00eac9fa8fbfa77ed98a5bd397ea
pengwenwu <pengwenwu@corp-ci.com>
2021-04-23 18:32:24 +0800

    add:day4一致性哈希

---
+++ gee/gee_cache/day4_consistent_hash/geekcache/consistenthash/consistenthash.go
@@ -0,0 +1,66 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+// Hash maps bytes to uint32
+type Hash func(data []byte) uint32
+
+// Map constains all hashed keys
+type Map struct {
+	hash     Hash           // 函数类型，依赖注入，允许用于替换成自定义的Hash函数
+	replicas int            // 虚拟节点倍数
+	keys     []int          // Sorted 哈希环
+	hashMap  map[int]string // 虚拟节点与真实节点的映射表，键：虚拟节点哈希值，值：真实节点名称
+}
+
+// New creates a Map instance
+// 构造函数：允许自定义虚拟节点倍数和Hash函数
+func New(relicas int, fn Hash) *Map {
+	m := &Map{
+		hash:     fn,
+		replicas: relicas,
+		hashMap:  make(map[int]string),
+	}
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
+
+// Add adds some keys to the hash
+// Add 函数允许传入0或者多个真实节点的名称
+// 对每一个真实节点key，对应创建m.replicas个虚拟节点，虚拟节点的名称通过添加编号的方式区分不同虚拟节点
+// 使用m.hash()计算虚拟节点的哈希值，追加到环上
+// hashMap中增加虚拟节点和真实节点的映射关系
+// 最后，环上的哈希值排序
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			m.keys = append(m.keys, hash)
+			m.hashMap[hash] = key
+		}
+	}
+}
+
+// Get gets the closest item in the hash to the provided key 在哈希环提供的key中查找最近的节点
+// 计算key的哈希值
+// 顺时针找到第一个匹配的虚拟节点的下标idx，从m.keys中获取到对应的哈希值。环状结构，取余
+// 通过hashMap映射获取到真实的节点
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+
+	hash := int(m.hash([]byte(key)))
+	// Binary search for appropriate replica 二分查找
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+
+	return m.hashMap[m.keys[idx%len(m.keys)]]
+}
---
+++ gee/gee_cache/day4_consistent_hash/geekcache/http.go
@@ -0,0 +1,61 @@
+package geekcache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultBasePath = "/_geecache/" // 节点间通讯地址前缀
+
+// HTTPPool implements PeerPicker for a pool of HTTP peers
+type HTTPPool struct {
+	// this peer's base URL, e.g. "https://expamle.net:8000"
+	self     string
+	basePath string
+}
+
+func NewHTTPPool(self string) *HTTPPool {
+	return &HTTPPool{
+		self:     self,
+		basePath: defaultBasePath,
+	}
+}
+
+// Log info with server name
+func (p *HTTPPool) Log(format string, v ...interface{}) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+// ServeHTTP handle all http requests
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool serving expected path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+	// /<basepath>/<groupname>/<key> required
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
+}
