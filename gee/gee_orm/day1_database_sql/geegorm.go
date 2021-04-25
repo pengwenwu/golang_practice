75a1ace8bdf8796627b75d8d90a80fdf557c0b94
pengwenwu <pengwenwu@corp-ci.com>
2021-04-25 18:20:14 +0800

    add:day1-database/sql基础

---
+++ gee/gee_orm/day1_database_sql/geegorm.go
@@ -0,0 +1,43 @@
+package geegorm
+
+import (
+	"database/sql"
+	"geeorm/session"
+
+	"geeorm/log"
+)
+
+type Engine struct {
+	db *sql.DB
+}
+
+func NewEngine(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	// Send a ping to make sure the database connection is alive
+	if err = db.Ping(); err != nil {
+		log.Error(err)
+		return
+	}
+	e = &Engine{
+		db: db,
+	}
+	log.Info("Connect database success")
+	return
+}
+
+func (e *Engine) Close() {
+	if err := e.db.Close(); err != nil {
+		log.Error("Failed to close databse")
+		return
+	}
+	log.Info("Close database success")
+}
+
+func (e *Engine) NewSession() *session.Session {
+	return session.New(e.db)
+}
