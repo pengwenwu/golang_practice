f13c2b8cdd55b12937073d655ad40e04ed0ed014
pengwenwu <pengwenwu@corp-ci.com>
2021-05-06 11:28:45 +0800

    add:day5实现钩子

---
+++ gee/gee_orm/day5_hooks/schema/schema.go
@@ -0,0 +1,67 @@
+package schema
+
+import (
+	"geeorm/dialect"
+	"go/ast"
+	"reflect"
+)
+
+// Field represents a column of database
+type Field struct {
+	Name string
+	Type string
+	Tag  string
+}
+
+// Schema represents a table of database
+type Schema struct {
+	Model      interface{}
+	Name       string
+	Fields     []*Field
+	FieldNames []string
+	fieldMap   map[string]*Field
+}
+
+func (s *Schema) GetField(name string) *Field {
+	return s.fieldMap[name]
+}
+
+// TypeOf() 和 ValueOf() 是reflect包最为基本也是最重要的2个方法，分别用来返回入参的类型和值。
+// 因为设计的入参是一个对象的指针，因此需要reflect.Indirect()获取指针指向的实例
+// modelType.Name() 获取到结构体的名称作为表名
+// NumField() 获取实例的字段的个数，然后通过下标获取到特定字段 p:= modelType.Field(i)
+// p.Name 即字段名，p.Type 即字段类型，通过 (Dialect).DataTypeOf() 转换为数据库字段类型，p.Tag 即额外的约束条件
+func Parse(dest interface{}, d dialect.Dialect) *Schema {
+	modelType := reflect.Indirect(reflect.ValueOf(dest)).Type()
+	schema := &Schema{
+		Model:    dest,
+		Name:     modelType.Name(),
+		fieldMap: make(map[string]*Field),
+	}
+
+	for i := 0; i < modelType.NumField(); i++ {
+		p := modelType.Field(i)
+		if !p.Anonymous && ast.IsExported(p.Name) {
+			field := &Field{
+				Name: p.Name,
+				Type: d.DataTypeOf(reflect.Indirect(reflect.New(p.Type))),
+			}
+			if v, ok := p.Tag.Lookup("geeorm"); ok {
+				field.Tag = v
+			}
+			schema.Fields = append(schema.Fields, field)
+			schema.FieldNames = append(schema.FieldNames, p.Name)
+			schema.fieldMap[p.Name] = field
+		}
+	}
+	return schema
+}
+
+func (s *Schema) RecordValues(dest interface{}) []interface{} {
+	destValue := reflect.Indirect(reflect.ValueOf(dest))
+	var fieldValues []interface{}
+	for _, field := range s.Fields {
+		fieldValues = append(fieldValues, destValue.FieldByName(field.Name).Interface())
+	}
+	return fieldValues
+}
